2b6c8545b6db328c240e7c01c5d2b93427ee79d8
kerem durabay <fkeremdurabay@gmail.com>
2023-02-19 14:48:24 +0300

    Update

---
+++ DataLayer/Migrations/20230218221134_initialContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    lastName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService UserService;
+        public UserController()
+        {
+            this.UserService = new UserService();
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return this.UserService.GetUserById(id);
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return this.UserService.AllUsers();
+        }
+
+        [HttpPost]
+        public bool Post([FromBody] User user) { 
+            return this.UserService.CreateUser(user);
+        }
+
+        [HttpPut]
+        public bool Put([FromBody] User user)
+        {
+            return this.UserService.UpdateUser(user);
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return this.UserService.DeleteUser(id);
+        }
+
+
+    }
+}
