1302726b09b08bc575be9916f87885adbae2c0fe
keremdurabay <fkeremdurabay@gmail.com>
2023-03-14 16:36:21 +0300

    dataLayer'a user modeli eklendi.

---
+++ DataLayer/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+    public string FullName
+    {
+        get { return string.Concat(FirstName, " ", LastName); }
+        private set { }
+    }
+}
